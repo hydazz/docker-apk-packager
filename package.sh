7acae0e398bbb126ea288c990436c5967d6e04d5
hydazz <53986978+hydazz@users.noreply.github.com>
2021-01-13 16:40:11 +1100

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,27 +2,35 @@
 #set -x
 # ./package.sh <arch> <path to key> <path to apkbuild> <path to output>
 
+# colours
+RED='\033[1;31m'   # echo Red
+BLUE='\033[1;34m'  # echo Blue
+GREEN='\033[0;32m' # echo Green
+BOLD='\033[1;37m'  # echo White Bold
+NC='\033[0m'       # echo No Colour
+
 # error out before continuing if something is not supplied
 if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] || [ -z $4 ]; then
+	echo -e "${RED}"
 	[[ -z $1 ]] &&
-		echo "Error: No arch is specified"
-
+		echo "Error: No build architecture specified"
 	[[ -z $2 ]] &&
 		echo "Error: No key location specified"
-
 	[[ -z $3 ]] &&
-		echo "Error: No APKBUILD path is specified"
-
+		echo "Error: No package path specified"
 	[[ -z $4 ]] &&
 		echo "Error: No output directory specified"
-	echo "Usage: ./package.sh <amd64/arm64/armhf> /path/to/key.rsa /path/to/apkbuild/folder/ /path/to/output"
+	echo -e "${NC}"
+	echo -e "${BOLD}Usage: ./package.sh <amd64/arm64/armv7> /path/to/key.rsa /path/to/package/ /path/to/output/${NC}"
 	exit 1
 fi
 if [ "$1" = "amd64" ] || [ "$1" = "arm64" ] || [ "$1" = "armv7" ]; then
 	:
 else
+	echo -e "${RED}"
 	echo "Error: $1 is not a supported architecture"
-	echo "Supported architectures: amd64, arm64, armhf"
+	echo -e "${BOLD}Supported architectures: amd64, arm64, armv7"
+	echo -e "${NC}"
 	exit 1
 fi
 
@@ -47,22 +55,32 @@ function build() {
 		${REPO}
 }
 
-echo "This script will package ${FOLDER_NAME} for ${ARCH} and export the .apk files to ${OUTPUT}, press Ctrl+C within 5 seconds to cancel"
+echo -e "${BLUE}This script will package ${FOLDER_NAME} for ${ARCH} and export the .apk files to ${OUTPUT}, press Ctrl+C within 5 seconds to cancel${NC}"
 sleep 5
 
 [[ ${ARCH} = "amd64" ]] &&
 	REPO=$AMD64
-
 [[ ${ARCH} = "arm64" ]] &&
 	REPO=$ARM64
-
-[[ ${ARCH} = "armhf" ]] &&
+[[ ${ARCH} = "armv7" ]] &&
 	REPO=$ARMV7
 
-build > build.log 2>&1
+echo -e "${BLUE}Packaging... This will take a while, see 'tail -f build.log' for a live build log${NC}"
+build >build.log 2>&1
 
 if cat build.log | grep -q "Build Failed"; then
-	echo "Error: Build failed. see 'cat build.log' for more information."
+	cat build.log
+	echo -e "${RED}Error: Build failed. see above for more information.${NC}"
+	FAILED=true
 else
-     echo "Done! Files saved to ${OUTPUT}."
+	echo -e "${GREEN}Done! Files saved to ${OUTPUT}.${NC}"
+fi
+
+# cleanup
+rm -rf build.log
+
+if [ "$FAILED" = "true" ]; then
+	exit 1
+else
+	exit 0
 fi
